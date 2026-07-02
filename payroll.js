320f6d8310c68f44fd17ef4dab0dd69db035da63
Lerato Yapi <leratoyapi482@gmail.com>
2026-07-03 00:59:51 +0200

    Prefix HTML/CSS classes and IDs; start JS + JSON for payroll page

--- payroll.js
+++ payroll.js
@@ -1,22 +1,71 @@
-let payroll_data 
-fetch()
-// payroll_data =
-
-// FOR THE PAYSLIP GENERATE BUTTON
-
-async function generate() {
-  constresponse = await fetch ('payroll_data.json');
-  const data = await Response.json();
-
-  document.getElementsById('name').innertext = data.name
-  document.getElementById('id').innertext = data.id
-  document.getElementsById('hoursWorked').innertext = data.hoursWorked
-  document.getElementsById('leaveDeductions').innertext = data.leaveDeductions
-  document.getElementsById('salary').innertext = data.salary
-
-  
-  const element = document.getElementByClass('payslip-template');
+// here i  will be fetching the data from the json file and then i  will be using that data to generate the payslip //
 
 
-  html2pdf().from(element).save('payslip.pdf');
+let pr_allPayroll = [];
+let pr_allEmployee = [];
+let pr_allAttendance=[]
+
+// console.log('Starting to load data from payroll_data.json...');
+
+fetch('data/employee_info.json')
+.then(response => response.json())
+.then(data => {
+  pr_allPayroll = data.payrollData;
+  console.log('SUCCESS! Payroll data loaded', pr_allPayroll.length);
+})
+   
+fetch('data/employee_info.json')
+.then(response => reponse.json())
+.then(data => {
+  allEmplyees = data.employeeData;
+  console.log('SUCCESS! Employee data loaded', allEmployees.lenght);
+})
+
+fetch('data/attendance.json')
+.then(response => response.json())
+.then(data => {
+  allAttendance = data.attendanceData;
+  console.log('SUCCESS! Attendance data loaded', allAttendance.lenght);
+
+})
+
+// here im combining the data using function and then i will  will be using that data to generate the payslip
+
+function combinneData(){
+  let combined= [];
+
+  // basically this will to find the employee's payroll info
+  for (let employee of allEmployee){
+    let payrollInfo = allPayroll.find(p => p.employeeId === employee.employeeId);
+
+    if (payrollInfo) {
+      combined.push({
+        id: employee.employeeId,
+        name: employee.name,
+        department: employee.department,
+        salary: employee.salary,
+        hoursWorked: payrollInfo.hoursWorked,
+        finalSalary: payrollInfo.finalSalary
+
+      })
+    }
+
+  }
+  return combined;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
