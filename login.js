e38b67357664c5be7865d22475df82c0345810b0
Elijah Lategan <elijahlategan3@gmail.com>
2026-07-09 14:35:36 +0200

    initial commit

---
+++ login.js
@@ -0,0 +1,45 @@
+const loginForm = document.getElementById("loginForm");
+
+
+loginForm.addEventListener("submit", function(e){
+
+
+    e.preventDefault();
+
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+
+    const message = document.getElementById("message");
+
+
+    if(username === "Elijah" && password === "lat123"){
+
+
+        message.style.color = "green";
+        message.innerHTML = "Login Successful!";
+
+
+        setTimeout(()=>{
+
+
+            // Takes you to dashboard page if correct credentials are implemented
+            window.location.href = "dashboard.html";
+
+
+        },1000);
+
+
+    }
+    else{
+
+
+        message.style.color="red";
+        message.innerHTML="Incorrect username or password.";
+
+
+    }
+
+
+});
